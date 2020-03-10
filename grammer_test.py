ee24dc0ddfbab8807858e0154268c1bc0e3377c5
xuqi <xuqi.cn@outlook.com>
2020-03-10 13:57:38 +0800

    初步实现了简单的感知机实例

---
+++ grammer_test.py
@@ -0,0 +1,15 @@
+'''
+@Project:MachineLearningAction
+
+@Author:lincoln
+
+@File:grammer_test
+
+@Time:2020-03-10 11:24:44
+
+@Description:
+'''
+import numpy as np
+a = np.array([1,2,3,4])
+b = np.array([1,2,3,4])
+print(a.dot(b))
